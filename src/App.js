0813190354b5b465af2a8d2bd64122504a65c714
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2021-11-27 14:31:25 +0330

    add logo component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
 		<Navigation />
+		<Logo />
     </div>
   );
 }
