386bbfe622c27d6dea70ff733f5f9cba12c0b491
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2021-11-27 16:42:41 +0330

    add Rank component and install particles

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
+import Rank from './components/Rank/Rank';
 
 function App() {
   return (
     <div className="App">
 		<Navigation />
 		<Logo />
+    <Rank />
 		<ImageLinkForm />
     </div>
   );
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Rank = () =>{
+    return(
+        <div>
+            <div className='f3'>
+                {'Ehsan your current rank is ...' }
+            </div>
+            <div className='f1'>
+                {'#5' }
+            </div>
+        </div>
+    );
+}
+export default Rank;
