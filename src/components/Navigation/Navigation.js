7a02a2d7d657f402b9541190cc98bcfff33d901f
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2021-11-27 14:00:52 +0330

    add navigation.js

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navigation = () => {
+    return(
+        <nav>
+            <p>
+                Sign out
+            </p>
+        </nav>
+    )
+}
+
+export default Navigation;
