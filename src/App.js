207fce808afb20155c109df6137969d44058a328
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2021-11-27 14:04:03 +0330

    add navigation to app and add some css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
-		
+		<Navigation />
     </div>
   );
 }
